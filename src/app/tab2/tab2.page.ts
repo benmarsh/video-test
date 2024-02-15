cbb2433558b81673894e7e513cab2df1a8214de8
benmarsh <ben+github@benmarsh.com>
2024-02-15 11:43:23 +0000

    working demo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  @ViewChild('videoElement') videoElement: any;
 
-  constructor() {}
+  private http = inject(HttpClient);
 
+  ngOnInit() {
+    this.http.get('assets/flower.mp4', {responseType: 'blob'}).subscribe((response) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const videoElement: HTMLVideoElement = this.videoElement.nativeElement;
+        videoElement.src = reader.result as string
+      }
+      reader.readAsDataURL(response);
+    });
+  }
 }
